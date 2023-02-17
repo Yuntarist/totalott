c3228f9a71bab38c519e2044870480598f295a3b
Yuntarist <ftaruftk@naver.com>
2023-02-17 14:48:11 +0900

    230217

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/2.About.vue')
+      import(/* webpackChunkName: "about" */ '../views/2.mainpage.vue')
   },
   {
     path: '/about3',
@@ -23,7 +23,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/3.About.vue')
+      import(/* webpackChunkName: "about" */ '../views/3.loginpage.vue')
   },
   {
     path: '/about4',
@@ -32,7 +32,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/4.About.vue')
+      import(/* webpackChunkName: "about" */ '../views/4.Newaccount.vue')
   },
   {
     path: '/about5',
