93f54a8070da585873b81e4620d28e759bc5cba6
Juminzz <justin4836@naver.com>
2023-02-20 17:42:13 +0900

    02.20

--- ori.js
+++ ori.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const USER = process.env.dbid;
-const PWD = process.env.dbpwd;
-const HOST = process.env.dbhost;
-const DB = "mdb";
+const USER = "ljm";
+const PWD = "12345";
+const HOST = "127.0.0.1:27017";
+const DB = "ljm";
 const mongodbURL = `mongodb://${USER}:${PWD}@${HOST}/${DB}`;
 // mongoose.set('useFindAndModify', false) // 6.0 이후부터는 자동관리
 mongoose.set("strictQuery", false); // 6.0 이후 권장사항
