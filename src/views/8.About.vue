4806affd2247fd359d57e9392f6f5e5c5efa02fb
Juminzz <justin4836@naver.com>
2023-03-13 17:44:44 +0900

    03.13

--- src/views/8.About.vue
+++ src/views/8.About.vue
@@ -6,74 +6,16 @@
     <div v-for="(item, i) in list" :key="i">
       <details class="alist">
         <summary>{{ item }}</summary>
-        <div v-for="(item2, a) in answer" :key="a">
-          <a href="#" class="alist"> {{ item2 }}</a>
+        <div>
+          <!-- 각각 배열에 담긴 주소 -->
+          <a :href="link[i]" target="_blank">{{ answer[i] }}</a>
         </div>
       </details>
     </div>
-
-    <!-- <div>
-      <details class="alist">
-        <summary>Gamesgate는 어떻게 설치하나요 ?</summary>
-        <a href="#">수정중</a>
-      </details>
-    </div>
-
-    <div>
-      <details class="alist">
-        <summary>Greengaming은 어떻게 설치하나요 ?</summary>
-        <a href="#">수정중</a>
-      </details>
-    </div>
-
-    <div>
-      <details class="alist">
-        <summary>Dreamgame은 어떻게 설치하나요 ?</summary>
-        <a href="#">수정중</a>
-      </details>
-    </div>
-    <div>
-      <a href="#" class="alist"
-        >활성화 키를 받으려면 계정을 만들어야 하나요 ?</a
-      >
-      <p></p>
-    </div>
-    <div>
-      <a href="#" class="alist">확인 메일을 받지 못했습니다.</a>
-      <p></p>
-    </div>
-    <div>
-      <a href="#" class="alist">비밀번호는 어떻게 변경하나요 ?</a>
-      <p></p>
-    </div>
-    <div>
-      <a href="#" class="alist">이메일은 어떻게 변경하나요 ?</a>
-      <p></p>
-    </div>
-    <div>
-      <a href="#" class="alist">내 계정 삭제하는 방법</a>
-      <p></p>
-    </div>
-    <div>
-      <a href="#" class="alist">게임이 작동하지 않습니다.</a>
-      <p></p>
-    </div>
-    <div>
-      <a href="#" class="alist">게임키란 ?</a>
-      <p></p>
-    </div>
-    <div>
-      <a href="#" class="alist">지불 방법</a>
-      <p></p>
-    </div>
-    <div>
-      <a href="#" class="alist">게임주문을 환불/취소할 수 있나요 ?</a>
-      <p></p>
-    </div> -->
   </div>
-  <div class="btn-circle">
+  <div class="btn-circle" @click="modalA()">
     <div class="bt-add">
-      <button class="circle" @click="dd"></button>
+      <button class="circle" @click="today"></button>
     </div>
     <div class="modal-background hide">
       <div class="modal-box">
@@ -81,16 +23,43 @@
           {{ date }}
           <br />
           <div class="modal-header">
-            <span>무엇을 도와드릴까요?</span>
+            <span class="sptime" style="display: none">
+              <div class="bot-all">
+                <img class="bot-img" src="../assets/qna.png" alt="TRIF" />
+                <span class="bot-text">고객센터</span><br />
+                <span style="color: black"
+                  >안녕하세요. 무엇을 도와드릴까요?</span
+                >
+              </div>
+            </span>
           </div>
-          <div class="qna-box">
-            <div><a href="#">게임문의</a></div>
-            <div><a href="#">결제문의</a></div>
-            <div><a href="#">계정문의</a></div>
-            <div><a href="#">버그문의</a></div>
-            <div><a href="#">할인문의</a></div>
+          <div class="qna-all" style="display: none">
+            <div class="qna-text" style="display: none">
+              <div class="qna-box" @click="qna1()">
+                <a href="#">게임문의</a>
+              </div>
+              <div class="qna-box" @click="qna2()">
+                <a href="#">결제문의</a>
+              </div>
+              <div class="qna-box" @click="qna3()">
+                <a href="#">계정문의</a>
+              </div>
+              <div class="qna-box" @click="qna4()">
+                <a href="#">버그문의</a>
+              </div>
+              <div class="qna-box" @click="qna5()">
+                <a href="#">할인문의</a>
+              </div>
+              <p style="color: blue">1개를 선택 해주세요</p>
+              <br />
+            </div>
           </div>
-          {{ test }}
+          <div class="answer" style="display: none">
+            {{ test }}
+          </div>
+          <div class="qna-result" style="display: none">
+            {{ qna }}
+          </div>
         </div>
         <div class="xmodal">
           <span class="x" @click="xx">X</span>
@@ -112,28 +81,57 @@ export default {
       list: [
         "Steam은 어떻게 설치하나요 ?",
         "Gamesgate는 어떻게 설치하나요 ?",
-        "Greengaming은 어떻게 설치하나요?",
+        "Greenmangaming 어떻게 설치하나요?",
         "Dreamgame은 어떻게 설치하나요 ?",
       ],
-      answer: ["steam 설치 사이트", "수정중", "수정중", "수정중"],
+      answer: [
+        "Steam 설치 사이트",
+        "Gamesgate 사이트",
+        "Greenmangaming 사이트",
+        "Dreamgame 사이트",
+      ],
+      link: [
+        "https://store.steampowered.com/about/",
+        "https://www.gamersgate.com/",
+        "https://www.greenmangaming.com/ko/all-games/on-sale/",
+        "https://www.dreamgame.com/",
+      ],
       dada: "",
       date: "",
       test: "",
+      qna: "",
     };
   },
-  mounted: function () {
-    const button = document.querySelector(".circle");
-    const modal = document.querySelector(".modal-background");
-    const x = document.querySelector(".x");
-    button.addEventListener("click", () => {
-      modal.style.display = "block";
-    });
-    x.addEventListener("click", () => {
-      modal.style.display = "none";
-    });
-  },
   methods: {
-    dd: function () {
+    modalA: function () {
+      const button = document.querySelector(".circle");
+      const modal = document.querySelector(".modal-background");
+      const answer = document.querySelector(".answer");
+      const sptime = document.querySelector(".sptime");
+      const qnaText = document.querySelector(".qna-text");
+      const qnaResult = document.querySelector(".qna-result");
+      const qnaAll = document.querySelector(".qna-all");
+      const x = document.querySelector(".x");
+
+      button.addEventListener("click", () => {
+        modal.style.display = "block";
+        setTimeout(() => {
+          sptime.style.display = "block";
+        }, 1000);
+        setTimeout(() => {
+          qnaAll.style.display = "block";
+          qnaText.style.display = "block";
+        }, 2000);
+      });
+      x.addEventListener("click", () => {
+        modal.style.display = "none";
+        answer.style.display = "none";
+        qnaResult.style.display = "none";
+        qnaAll.style.display = "none";
+      });
+    },
+
+    today: function () {
       let today = new Date();
       var year = today.getFullYear();
       var month = ("0" + (today.getMonth() + 1)).slice(-2);
@@ -141,14 +139,38 @@ export default {
       var dateString = year + "년" + month + "월" + day + "일";
       this.date = dateString;
     },
-    ds: function () {
-      this.test = "수정중";
+    qna1: function () {
+      const answer = document.querySelector(".answer");
+      const qnaResult = document.querySelector(".qna-result");
+      answer.style.display = "block";
+      this.test = "게임문의";
+      setTimeout(() => {
+        qnaResult.style.display = "block";
+        this.qna = "수정중";
+      }, 2000);
     },
-    da: function () {
-      this.test = "수정중";
+    qna2: function () {
+      const answer = document.querySelector(".answer");
+      answer.style.display = "block";
+      this.test = "결제문의";
     },
-    xx: function () {
-      this.test = "";
+    qna3: function () {
+      const answer = document.querySelector(".answer");
+      answer.style.display = "block";
+      this.test = "계정문의";
+    },
+    qna4: function () {
+      const answer = document.querySelector(".answer");
+      answer.style.display = "block";
+      this.test = "버그문의";
+    },
+    qna5: function () {
+      const answer = document.querySelector(".answer");
+      answer.style.display = "block";
+      this.test = "할인문의";
+    },
+    ad: function () {
+      href = "https://naver.com";
     },
   },
 };
